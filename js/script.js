9f9f953ae950cdd755cb94265f68677a9abae710
SACM <sergiomoya757@gmail.com>
2024-06-25 20:49:24 -0500

    Corrección HTML, CSS, Assets y optimización JS.

--- js/script.js
+++ js/script.js
@@ -1,52 +1,41 @@
-function getRandomColor() {
-    // Generar un color hexadecimal aleatorio
-    return '#' + Math.floor(Math.random()*16777215).toString(16);
+document.addEventListener("DOMContentLoaded", function () {
+  let header = document.querySelector("header");
+  let h1 = document.querySelector(".random-color");
+  function generarColorAleatorio() {
+    // Generar colores aleatorios en formato hexadecimal
+    let color1 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    let color2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    let color3 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    // Actualiza los colores
+    header.style.backgroundImage =
+      "linear-gradient(to right, " + color1 + ", " + color2 + ")";
+    h1.style.color = color3;
   }
-  
-  function applyRandomColor() {
-    var h1 = document.querySelector('.random-color');
-    var randomColor = getRandomColor();
-    h1.style.color = randomColor; // Aplicar el color aleatorio al elemento h1
+  setInterval(generarColorAleatorio, 800);
+});
+// Seleccionar el contenedor de imágenes y todas las imágenes dentro de él
+// Obtener la cantidad total de imágenes
+// Obtener la cantidad total de imágenes
+// Obtener el ancho de la primera imagen
+// Inicializar el índice actual y el ancho total del deslizamiento (tres imágenes juntas)
+// Ancho total de las imágenes a mostrar
+const imagesContainer = document.querySelector(".images");
+const images = document.querySelectorAll(".images img");
+const numImages = images.length;
+const imageWidth = images[0].clientWidth;
+let currentIndex = 0;
+const slideWidth = imageWidth * 3;
+document.querySelector(".prev").addEventListener("click", () => {
+  currentIndex -= slideWidth; // Disminuir el índice actual por el ancho total del deslizamiento
+  if (currentIndex < 0) {
+    currentIndex = imageWidth * (numImages - 3); // Si el índice actual es menor que 0, establecerlo para mostrar las últimas tres imágenes
   }
-  
-  // Cambiar el color aleatorio cada 0.18 segundos
-  setInterval(applyRandomColor, 780);
-
-  document.addEventListener("DOMContentLoaded", function() {
-    var header = document.querySelector('header');
-  
-    function generarColorAleatorio() {
-      // Generar colores aleatorios en formato hexadecimal
-      var color1 = '#' + Math.floor(Math.random()*16777215).toString(16);
-      var color2 = '#' + Math.floor(Math.random()*16777215).toString(16);
-      // Actualizar el fondo con el nuevo degradado aleatorio
-      header.style.backgroundImage = 'linear-gradient(to right, ' + color1 + ', ' + color2 + ')';
-    }
-  
-    setInterval(generarColorAleatorio, 800); // Cambiar cada 5 segundos
-  });
-  const imagesContainer = document.querySelector('.images');
-  const images = document.querySelectorAll('.images img');
-  const numImages = images.length;
-  const imageWidth = images[0].clientWidth;
-  let currentIndex = 0;
-  const slideWidth = imageWidth * 3; // Ancho total de las imágenes a mostrar
-  const speed = 1; // Velocidad de desplazamiento (en píxeles por fotograma)
-  
-  document.querySelector('.prev').addEventListener('click', () => {
-      currentIndex -= slideWidth;
-      if (currentIndex < 0) {
-          currentIndex = imageWidth * (numImages - 1);
-      }
-      imagesContainer.style.transform = `translateX(-${currentIndex}px)`;
-  });
-  
-  document.querySelector('.next').addEventListener('click', () => {
-      currentIndex += slideWidth;
-      if (currentIndex > imageWidth * (numImages - 1)) {
-          currentIndex = 0;
-      }
-      imagesContainer.style.transform = `translateX(-${currentIndex}px)`;
-  });
-  
-  
+  imagesContainer.style.transform = `translateX(-${currentIndex}px)`; // Aplicar el desplazamiento horizontal al contenedor de imágenes
+});
+document.querySelector(".next").addEventListener("click", () => {
+  currentIndex += slideWidth; // Aumentar el índice actual por el ancho total del deslizamiento
+  if (currentIndex > imageWidth * (numImages - 1)) {
+    currentIndex = 0; // Si el índice actual es mayor que el ancho total de todas las imágenes, restablecer a 0
+  }
+  imagesContainer.style.transform = `translateX(-${currentIndex}px)`; // Aplicar el desplazamiento horizontal al contenedor de imágenes
+});
