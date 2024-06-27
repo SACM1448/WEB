6e6435212704a6d567bc6caeee45dc4991082435
SACM <sergiomoya757mail.com>
2024-06-27 11:28:18 -0500

    Correción API

--- js/script.js
+++ js/script.js
@@ -22,23 +22,32 @@ document.addEventListener("DOMContentLoaded", function () {
 const imagesContainer = document.querySelector(".images");
 const images = document.querySelectorAll(".images img");
 const numImages = images.length;
-const imageWidth = images[0].clientWidth;
-let currentIndex = 0;
-const slideWidth = imageWidth * 3;
+const anchoImg = images[0].clientWidth;
+let indice = 0;
+const anchoTotal = anchoImg * 3;
 document.querySelector(".prev").addEventListener("click", () => {
-  currentIndex -= slideWidth; // Disminuir el índice actual por el ancho total del deslizamiento
-  if (currentIndex < 0) {
-    currentIndex = imageWidth * (numImages - 3); // Si el índice actual es menor que 0, establecerlo para mostrar las últimas tres imágenes
+  indice -= anchoTotal; // Disminuir el índice actual por el ancho total del deslizamiento
+  if (indice < 0) {
+    indice = anchoImg * (numImages - 3); // Si el índice actual es menor que 0, establecerlo para mostrar las últimas tres imágenes
   }
-  imagesContainer.style.transform = `translateX(-${currentIndex}px)`; // Aplicar el desplazamiento horizontal al contenedor de imágenes
+  imagesContainer.style.transform = `translateX(-${indice}px)`; // Aplicar el desplazamiento horizontal al contenedor de imágenes
 });
 document.querySelector(".next").addEventListener("click", () => {
-  currentIndex += slideWidth; // Aumentar el índice actual por el ancho total del deslizamiento
-  if (currentIndex > imageWidth * (numImages - 1)) {
-    currentIndex = 0; // Si el índice actual es mayor que el ancho total de todas las imágenes, restablecer a 0
+  indice += anchoTotal; // Aumentar el índice actual por el ancho total del deslizamiento
+  if (indice > anchoImg * (numImages - 1)) {
+    indice = 0; // Si el índice actual es mayor que el ancho total de todas las imágenes, restablecer a 0
   }
-  imagesContainer.style.transform = `translateX(-${currentIndex}px)`; // Aplicar el desplazamiento horizontal al contenedor de imágenes
+  imagesContainer.style.transform = `translateX(-${indice}px)`; // Aplicar el desplazamiento horizontal al contenedor de imágenes
 });
+
+function mostrarMensaje(texto) {
+  const mensaje = document.getElementById('mensaje');
+  mensaje.textContent = texto;
+
+  setTimeout(() => {
+      mensaje.textContent = '';
+  }, 5000);
+}
 
 document.getElementById('file-input').addEventListener('change', (event) => {
   const fileInput = event.target;
@@ -75,16 +84,20 @@ document.getElementById('upload-form').addEventListener('submit', async (event) 
       }
 
       const data = await response.json();
-
-      // Mostrar mensaje emergente
-      alert('Datos enviados correctamente');
-
+      
+    // Llama a la función para mostrar el mensaje
+    mostrarMensaje('Datos enviados correctamente');
       // Volver a mostrar el logo de la compañía
       const previewImage = document.getElementById('preview-image');
       previewImage.src = './assets/IMG/logo.png';
       previewImage.alt = 'Logo compañía';
+      fileInput.value = '';
   } catch (error) {
       console.error('Error:', error);
-      alert('Error al enviar los datos');
+      mostrarMensaje('Error al enviar los datos');
+      const previewImage = document.getElementById('preview-image');
+      previewImage.src = './assets/IMG/logo.png';
+      previewImage.alt = 'Logo compañía';
+      fileInput.value = '';
   }
 });
